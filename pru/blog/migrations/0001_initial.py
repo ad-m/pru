3ec2e809cbce7862c1bbd27a0df92fc63dc6c471
Adam Dobrawy <naczelnik@jawnosc.tk>
2015-04-12 17:27:59 +0200

    Recreate migrations for blog and registers for postgresql support

--- pru/blog/migrations/0001_initial.py
+++ pru/blog/migrations/0001_initial.py
@@ -8,7 +8,7 @@ import autoslug.fields
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('registers', '0001_initial'),
+        ('registers', '__first__'),
     ]
 
     operations = [
@@ -18,7 +18,9 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('slug', autoslug.fields.AutoSlugField(unique=True, editable=False)),
                 ('img', models.ImageField(upload_to=b'', null=True, verbose_name='Featured image', blank=True)),
-                ('title', models.CharField(max_length=150, verbose_name='Institution name')),
+                ('title', models.CharField(max_length=150, verbose_name='Post title')),
+                ('public', models.BooleanField(default=False, help_text='Mark to publish on site', verbose_name='Public')),
+                ('comment', models.TextField(verbose_name='Comment', blank=True)),
                 ('created', models.DateTimeField(auto_now_add=True, verbose_name='Creation date')),
                 ('modified', models.DateTimeField(help_text='Date of last modification of this object', verbose_name='Last modification date', auto_now=True)),
                 ('registers', models.ManyToManyField(to='registers.Register', null=True, blank=True)),
@@ -26,5 +28,22 @@ class Migration(migrations.Migration):
             options={
             },
             bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Tag',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('slug', autoslug.fields.AutoSlugField(unique=True, editable=False)),
+                ('title', models.CharField(max_length=150, verbose_name='Tag name')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='post',
+            name='tags',
+            field=models.ManyToManyField(to='blog.Tag', null=True, blank=True),
+            preserve_default=True,
         ),
     ]
